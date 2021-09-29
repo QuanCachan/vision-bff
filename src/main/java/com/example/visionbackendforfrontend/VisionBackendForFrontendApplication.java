2a0f3cabff33a9b10076c88272da60d3a9b6e094
mqtran <minh-quan.tran@cacd2.fr>
2021-09-29 22:49:32 +0200

    init commit

---
+++ src/main/java/com/example/visionbackendforfrontend/VisionBackendForFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.visionbackendforfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionBackendForFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionBackendForFrontendApplication.class, args);
+    }
+
+}
